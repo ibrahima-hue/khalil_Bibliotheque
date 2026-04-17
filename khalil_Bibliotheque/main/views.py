b9f8d1067895c92509c1e80cff501708dfac7d6b
Ibrahima Ndao <ibrahima@cat.edu.sn>
2026-04-17 12:16:29 +0000

    Les templates et urls

--- khalil_Bibliotheque/main/views.py
+++ khalil_Bibliotheque/main/views.py
@@ -1,3 +1,9 @@
+from urllib import request
+
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+def contact(request):
+    return render(request, 'diko.html')
